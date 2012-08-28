8c1068edc79309e1ae79745766fb79a921086140
MARCO LOPEZ <mlopez@virtualsip.net>
2012-08-28 00:39:33 -0500

    cambio en pedidos
    lista y eliminar: agregar variable para las notificaciones
    guardar: creado el nuevo pedido del cliente, se redirecciona a la lista
    de sus pedidos

--- sis_coesa/ventas/pedidos/guardar_cliente.php
+++ sis_coesa/ventas/pedidos/guardar_cliente.php
@@ -6,17 +6,24 @@ require_once("../../../connect/sesion/verificar_sesion.php");
 
 //VARIABLES
 $pedido_cliente=$_POST["pedido_cliente"];
+$codigo_unico=codigoAleatorio(20,true,true,false);
 
 //GUARDAR
-$rst_guardar=mysql_query("INSERT INTO syCoesa_pedidos (id_cliente)
-VALUES ($pedido_cliente)", $conexion);
+$rst_guardar=mysql_query("INSERT INTO syCoesa_pedidos (id_cliente, cod_unico)
+VALUES ($pedido_cliente, '$codigo_unico')", $conexion);
+
+//EXTRAER ID DEL NUEVO CLIENTE
+$rst_cod=mysql_query("SELECT * FROM syCoesa_pedidos WHERE cod_unico='$codigo_unico' LIMIT 1;", $conexion);
+$fila_cod=mysql_fetch_array($rst_cod);
+$codId=$fila_cod["id_pedido"];
+$codCliente=$fila_cod["id_cliente"];
 
 if (mysql_errno()!=0){
-	echo "ERROR: ". mysql_errno() . " - ". mysql_error();
 	mysql_close($conexion);
+	header("Location:lista.php?m=2");
 } else {
 	mysql_close($conexion);
-	header("Location:lista.php?m=1");
+	header("Location:articulos/lista.php?id=".$codId."&clt=".$codCliente."");
 }
 
 ?>
